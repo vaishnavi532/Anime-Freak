d6f8f2ffb6edaab4790af6e388dbc344b4a958b4
Vaishnavi H S <136379124+vaishnavi532@users.noreply.github.com>
2023-11-03 21:26:09 +0530

    Add files via upload

---
+++ one.js
@@ -0,0 +1,14 @@
+$(".owl-carousel").owlCarousel({
+    //loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      800: {
+        items: 1
+      }
+    }
+  });
+  
